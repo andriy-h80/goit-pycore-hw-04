c4f6ddd0b7e29e0e90bbdd8383e831468d005773
andriy-h80 <andrey.hanzel@gmail.com>
2025-06-18 22:46:06 +0300

    task 1 almost done

--- task 1.py
+++ task 1.py
@@ -1,1 +1,38 @@
 
+'''
+Є текстовий файл, який містить інформацію
+про місячні заробітні плати розробників у вашій компанії.
+Кожен рядок у файлі містить прізвище розробника та його заробітну плату,
+які розділені комою без пробілів.
+'''
+
+# Alex Korp,3000
+# Nikita Borisenko,2000
+# Sitarama Raju,1000
+
+with open("salary_file.txt", "w", encoding='utf-8') as fh:
+    fh.write("Alex Korp,3000\n")
+    fh.write("Nikita Borisenko,2000\n")
+    fh.write("Sitarama Raju,1000\n")
+
+def total_salary(path):
+    with open("salary_file.txt", "r", encoding='utf-8') as fh:
+        total = 0
+        quantity = 0
+        for line in fh:
+            name, salary = line.split(',')
+            total += int(salary)
+            quantity += 1
+        
+        if quantity == 0:
+            return (0, 0)
+        
+        average = total / quantity
+        return (total, average)
+
+# Обробка вийнятків!!!!
+
+total, average = total_salary("path/to/salary_file.txt")
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+
+# Загальна сума заробітної плати: 6000, Середня заробітна плата: 2000
