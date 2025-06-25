6b8c0ca83a7576bf1d078db4f8d884b166ae45a9
andriy-h80 <andrey.hanzel@gmail.com>
2025-06-25 13:18:20 +0300

    task 3 done

--- task_3/task_3.py
+++ task_3/task_3.py
@@ -6,3 +6,37 @@
 Для кращого візуального сприйняття, імена директорій та файлів мають відрізнятися за кольором.
 '''
 
+import sys
+from pathlib import Path
+from colorama import Fore, Style
+
+def print_tree(path: Path, line_prefix: str = ""):
+    if not path.exists():
+        print(Fore.RED + f"Помилка: шлях '{path}' не існує." + Style.RESET_ALL)
+        return
+
+    print(f"{line_prefix}{Fore.BLUE} \U0001F4C2 {path.name}{Style.RESET_ALL}")
+
+    folders = []
+    files = []
+
+    for item in path.iterdir():
+        if item.is_dir():
+            folders.append(item)
+        else:
+            files.append(item)
+
+    for folder in folders:
+        print_tree(folder, line_prefix + " ┃ ")
+
+    for file in files:
+        print(f"{line_prefix} ┃ {Fore.YELLOW} \U0001F4DC {file.name}{Style.RESET_ALL}")
+
+
+if len(sys.argv) > 1:
+     path_arg = sys.argv[1]
+else:
+    path_arg = "."
+
+path = Path(path_arg)
+print_tree(path)
